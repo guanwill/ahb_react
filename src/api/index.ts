03bdd2a49e8f03d5a0e09241b9c46c81fc15f09e
guanwill <w.guan@hotmail.com>
2021-10-05 00:09:30 +1100

    new backend url wip

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,19 +1,19 @@
 import AuthApi from './auth/authApi';
-import GameApi from './games/gameApi'
+import GameApi from './games/gameApi';
 import GiantBombApi from './games/giantBombApi';
 import httpClient from './httpClient';
 
 export interface IApi {
-    authApi: AuthApi
-    gameApi: GameApi
-    giantBombApi: GiantBombApi
+    authApi: AuthApi;
+    gameApi: GameApi;
+    giantBombApi: GiantBombApi;
 }
 
 export default {
     // use localhost:8000 for local
-    authApi: new AuthApi(httpClient, 'https://abh-backend.herokuapp.com'),
-    gameApi: new GameApi(httpClient, 'https://abh-backend.herokuapp.com'),
-    giantBombApi: new GiantBombApi(httpClient, 'https://abh-backend.herokuapp.com')
+    authApi: new AuthApi(httpClient, 'http://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com'),
+    gameApi: new GameApi(httpClient, 'http://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com'),
+    giantBombApi: new GiantBombApi(httpClient, 'http://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com')
     // authApi: new AuthApi(httpClient, 'http://localhost:8000'),
     // gameApi: new GameApi(httpClient, 'http://localhost:8000'),
     // giantBombApi: new GiantBombApi(httpClient, 'http://localhost:8000')
