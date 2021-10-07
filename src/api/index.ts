2c9326373a8aa081da78ee5075256708bb1a34f2
guanwill <w.guan@hotmail.com>
2021-10-07 21:35:30 +1100

    updated mglbackend url

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,9 +11,12 @@ export interface IApi {
 
 export default {
     // use localhost:8000 for local
-    authApi: new AuthApi(httpClient, 'http://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com'),
-    gameApi: new GameApi(httpClient, 'http://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com'),
-    giantBombApi: new GiantBombApi(httpClient, 'http://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com')
+    authApi: new AuthApi(httpClient, 'https://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com'),
+    gameApi: new GameApi(httpClient, 'https://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com'),
+    giantBombApi: new GiantBombApi(
+        httpClient,
+        'https://mglbackend-env.eba-ruq68nam.ap-southeast-2.elasticbeanstalk.com'
+    )
     // authApi: new AuthApi(httpClient, 'http://localhost:8000'),
     // gameApi: new GameApi(httpClient, 'http://localhost:8000'),
     // giantBombApi: new GiantBombApi(httpClient, 'http://localhost:8000')
